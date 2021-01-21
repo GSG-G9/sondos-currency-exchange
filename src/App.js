5b452f2f84032d27cf3e8da099215f6604c004be
Sondos-Alalami <sondos.alalami@gmail.com>
2021-01-21 15:03:18 +0200

    adding some style

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <a href="index.html" class="logo_link"><p class="logo">Currency <span class="xchange_xletter">X </span>change</p></a>
       </header>
       <Countries/>
       
